cbc2241cc8abce3c8dcf60f83827933b1e1fe362
Dinh Phu <ndp17hp@gmail.com>
2020-10-14 17:26:27 +0700

    first commit

---
+++ src/main/java/com/supportpotal/SupportpotalApplication.java
@@ -0,0 +1,13 @@
+package com.supportpotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportpotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportpotalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supportpotal/SupportpotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportpotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportpotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
